df6b2daba0b8dc6e9b31bbc09ca0cc0774d79ce3
Cmnraja <natarajan.c97@gmail.com>
2017-06-17 11:29:14 +0530

    Create SplitFunction.java

---
+++ SplitFunction.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+		Scanner s =new Scanner(System.in);//10-03-1997
+		String a =s.next();
+		String[] part = a.split("-");
+		String part1 =part[2];
+		System.out.println(part1);//1997
+		
+	}
+}
