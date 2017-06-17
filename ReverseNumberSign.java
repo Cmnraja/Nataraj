73b19e3224b789cec7b218f64860d3452a6e70db
Cmnraja <natarajan.c97@gmail.com>
2017-06-17 10:04:13 +0530

    Create ReverseNumberSign.java

---
+++ ReverseNumberSign.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+		Scanner s =new Scanner(System.in);
+		int a =s.nextInt();
+		a=a*-1;
+		System.out.println(a);
+
+	}
+}
