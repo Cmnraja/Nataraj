6e86d82dec2568da012fd10713d909285f1a3a2c
Cmnraja <natarajan.c97@gmail.com>
2017-06-19 13:14:50 +0530

    Create RupeeToPaise.java

---
+++ RupeeToPaise.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+		Scanner s =new Scanner(System.in);
+		float a=s.nextFloat();
+		a=a*100;
+		System.out.println((int)a);
+
+	}
+}
