39156275d1945c4a97376e8524c5c5fb7bb00dd8
Cmnraja <natarajan.c97@gmail.com>
2017-06-19 10:35:27 +0530

    Create HeadCount-BirdsandAnimals.java

---
+++ HeadCount-BirdsandAnimals.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+		int x,y,a,b;
+		Scanner s =new Scanner(System.in);
+		x=s.nextInt();
+		y=s.nextInt();
+		a = ((2*x)-y)/2;
+		b = ((4*x)-y)/2;
+		System.out.println(-a+" "+b);
+
+	}
+}
