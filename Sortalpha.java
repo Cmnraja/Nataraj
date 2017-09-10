ff8c80039e58d3cf38326b2dfddd567dee9488dd
Cmnraja <natarajan.c97@gmail.com>
2017-09-10 15:18:05 +0530

    Create Sortalpha.java

---
+++ Sortalpha.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+		Scanner s =new Scanner(System.in);
+		String a=s.next();
+		String b=s.next();
+		System.out.print(a.charAt(0)>b.charAt(0)?b+a:a+b);
+
+	}
+}
+//Use collection 
+//Arraylist +Collection.sort
+//TreeSet
